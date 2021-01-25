3c015bbced7f17a6cc84b9dd534849061b21a829
Temp <temp@miniserver.local>
2021-01-25 17:20:16 +1100

    user authentication complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_070438) do
+ActiveRecord::Schema.define(version: 2021_01_25_043651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_070438) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
