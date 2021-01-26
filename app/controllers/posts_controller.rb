83d5d7b50ee7ed316ad48978593361de42dce297
Ken <plantdink@gmail.com>
2021-01-26 16:32:50 +1100

    Embedded comments completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,17 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create post_params
-    redirect_to post_path(post.id)
+    redirect_to post
   end
 
   def edit
+    @post = Post.find params[:id]
+  end
+
+  def update
+    post = Post.find params[:id]
+    post.update post_params
+    redirect_to post
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
   resources :users, :only => [:new, :create]
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
