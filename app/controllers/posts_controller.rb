1302dbb5a6674a95f68169ca0b05dbac0f633e6a
Ken <plantdink@gmail.com>
2021-01-26 13:22:40 +1100

    Post resources complete

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create post_params
+    redirect_to post_path(post.id)
+  end
+
+  def edit
+  end
+
+  def show
+    @post = Post.find params[:id]
+  end
+
+  def destroy
+    post = Post.find params[:id]
+    post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :example, :content)
+  end
+end
