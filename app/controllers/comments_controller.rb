9309d1cb3bf50091ce34489f296acf9ca42cffa6
Ken <plantdink@gmail.com>
2021-01-27 20:37:32 +1100

    more styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find params[:post_id]
     @comment = @post.comments.create(comment_params)
+    @current_user.comments << @comment
     redirect_to post_path(@post)
   end
 
