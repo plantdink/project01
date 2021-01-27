616f42e348cc804b680d26a9b0802dad620b839e
Ken <plantdink@gmail.com>
2021-01-27 17:50:33 +1100

    Authenticated user controls visible

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   def create
-    post = Post.find params[:post_id]
-    @comment = post.comments.create(comment_params)
-    redirect_to post
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
   end
 
   def destroy
